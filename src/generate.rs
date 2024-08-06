29c74f83ed9d9b8998e49b4618c31b72c0f8e42e
John Murray <me@johnmurray.io>
2024-08-06 11:19:53 -0400

    zip generated contents into a jar

--- src/generate.rs
+++ src/generate.rs
@@ -1,24 +1,38 @@
 use std::path::Path;
-use std::io::Write;
+use std::io::prelude::*;
 use std::fs;
-
+use std::fs::File;
 use crate::parse::ParseResult;
 
 use log::{debug, warn};
 use tempdir::TempDir;
+use walkdir::{DirEntry, WalkDir};
+use zip::DateTime;
+use zip::write::SimpleFileOptions;
 
 #[derive(Debug)]
 pub enum GenerateError {
     InvalidOutputPath(String),
     IoError(std::io::Error),
+    ZipError(String),
 }
 
 type Result<T> = std::result::Result<T, GenerateError>;
 
 pub fn generate_code(parse_result: ParseResult, output_path: &str) -> Result<()> {
     let output_path = validate_output_path(output_path)?;
+    let working_dir = init_working_directory()?;
     debug!("Writing to output path: {}", output_path);
+
     // TODO: Generate code
+
+    // TODO: Package the generated code into a source JAR
+    let output_file = File::create(Path::new(&output_path)).unwrap();
+    let walkdir = WalkDir::new(working_dir.path());
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), working_dir.path(), output_file)?;
+
     Ok(())
 }
 
@@ -68,4 +82,49 @@ fn init_working_directory() -> Result<TempDir> {
         .map_err(|e| GenerateError::IoError(e))?;
 
     Ok(dir)
+}
+
+fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &Path,
+    writer: T,
+) -> Result<()>
+where
+        T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = SimpleFileOptions::default()
+        .last_modified_time(DateTime::from_date_and_time(1980, 1, 1, 0, 0, 0).unwrap());
+
+    let prefix = Path::new(prefix);
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(prefix).unwrap();
+        let path_as_string = name
+            .to_str()
+            .map(str::to_owned)
+            .ok_or(GenerateError::ZipError(format!("Path is not valid UTF-8: {:?}", &name)))?;
+
+        if path.is_file() {
+            debug!("adding file {path:?} as {name:?} ...");
+            zip.start_file(path_as_string, options)
+                .map_err(|e| GenerateError::ZipError(format!("Failed to start file: {}", e)))?;
+            let mut f = File::open(path)
+                .map_err(|e| GenerateError::ZipError(format!("Failed to open file: {}", e)))?;
+            f.read_to_end(&mut buffer)
+                .map_err(|e| GenerateError::ZipError(format!("Failed to read file: {}", e)))?;
+            zip.write_all(&buffer)
+                .map_err(|e| GenerateError::ZipError(format!("Failed to write file: {}", e)))?;
+            buffer.clear();
+        } else if !name.as_os_str().is_empty() {
+            // If not a file (and not the root), then it must be a directory
+            zip.add_directory(name.to_str().unwrap(), options)
+                .map_err(|e| GenerateError::ZipError(format!("Failed to add directory: {}", e)))?;
+        }
+    }
+
+    zip.finish()
+        .map_err(|e| GenerateError::ZipError(format!("Failed to finish zip file: {}", e)))?;
+    Ok(())
 }
